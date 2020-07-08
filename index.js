3f2739de66954fd3d422af9e793d19eb04d71b36
Alexander Bazo <alexanderbazo@googlemail.com>
2020-07-08 13:08:55 +0200

    Add comments

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+// Importiert die vom PhraseGenerator bereitgestellte Funktion in dieses Modul
 import { getRandomPhrase } from "./PhraseGenerator.js";
 
+// Wählt eine zufällige Hello World-Phrase aus und gibt diese auf der Konsole aus
 function printRandomPhrase() {
-	let phrase = getRandomPhrase();
-	console.log(phrase);
+    let phrase = getRandomPhrase();
+    console.log(phrase);
 }
 
 // Liest das erste übergeben Kommandozeilenargument aus (argv[0] und argv[1] sind "node" und der Name der ausgeführten Datei)
 let numberOfPhrases = process.argv[2];
 
-for(let i = 0; i < numberOfPhrases; i++) {
-	printRandomPhrase();
+// Gibt n zufällige Hello World-Phrasen aus, die Anzahl wird durch den übergebenen Kommandozeilenargument bestimmt
+for (let i = 0; i < numberOfPhrases; i++) {
+    printRandomPhrase();
 }
